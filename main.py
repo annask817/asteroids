ccc2066031fc9d21880522523d3b495fe54d2a6d
Anna Kong <144516813+annask817@users.noreply.github.com>
2025-08-25 22:41:42 -0500

    main initialize and circleshape

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ from constants import *
 
 def main():
     pygame.init()
-    pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(pygame.display.get_surface(), (0, 0, 0), rect=None, special_flags=0)
+        screen.fill("black")
         pygame.display.flip()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+
+        # Limit framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
